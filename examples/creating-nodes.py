81bda8d2efff5e808067e4a82ed2625f5d99fe64
Rada Hernandez <rada@gruporlh.com>
2017-10-15 22:11:13 -0500

    Avances de las pruebas de tensor flow

--- examples/creating-nodes.py
+++ examples/creating-nodes.py
@@ -39,6 +39,22 @@ b = tf.placeholder(tf.float32)
 adder_node = a + b
 
 """ Printing the results of the above function """
-print(sess.run(adder_node, { a:59, b:852 }))
-print(sess.run(adder_node, {a:[4,5], b:[584,1256]}))
+print(sess.run(adder_node, { a:3, b:4.5 }))
+print(sess.run(adder_node, {a:[1,3], b:[2,4]}))
 
+
+add_and_triple = adder_node * 3
+print(sess.run(add_and_triple, {a:3, b:4.5}))
+
+
+W = tf.Variable([.3], dtype=tf.float32)
+b = tf.Variable([-.3], dtype=tf.float32)
+x = tf.placeholder(tf.float32)
+
+linear_model = W * x + b
+
+init = tf.global_variables_initializer()
+
+sess.run(init)
+
+print(sess.run(linear_model, {x: [1, 2, 3, 4]}))
